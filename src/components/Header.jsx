0815af8e7a9bdd8e18a733689052809881e4a677
majdrammal <majdrammal2001@gmail.com>
2022-09-20 11:23:41 +0300

    Added button events + styling changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,12 +8,8 @@ const Header = () => {
 
     function changeWord() {
         setInterval(function() {
-            document.querySelector(".header__slogan").innerHTML = `We ${array[i]} Steel`
+            document.querySelector(".header__slogan").innerHTML = `We ${array[i % 3]} Steel`
             i++
-            console.log(i)
-            if (i > 2) {
-                i = 0
-            }
         }, 2000);
     }
 
@@ -25,7 +21,7 @@ const Header = () => {
         <div id="header">
             <div className="container">
                 <h1><span className="large">Solid</span> Contracting</h1>
-                <h2 className="header__slogan"></h2>
+                <h2 className="header__slogan">We trade steel</h2>
                 <img src={bg} alt="" />
             </div>
         </div>
--- src/components/ui/Service.jsx
+++ src/components/ui/Service.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 
-const Service = () => {
+const Service = ({ num }) => {
+
+    let desc = false
+    function showDescription() {
+
+        if (!desc) {
+            document.querySelector(`.service__${num}`).classList += " service__para--on"
+            desc = true
+        }
+
+        else {
+            document.querySelector(`.service__${num}`).classList.remove("service__para--on") 
+            desc = false
+        }
+    }
+
     return (
-        <div className="service">
+        <div className={`service service__${num}`}>
             <img src="https://worldsteel.org/wp-content/uploads/steel-tubes.jpg"alt="" />
+            <p className="service__para" >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Distinctio accusantium labore maiores veritatis. Et quod maiores facere enim ex nostrum.</p>
             <h3>Steel</h3>
-            <button>Learn More</button>
+            <button onClick={showDescription}>Learn More</button>
         </div>
     );
 }
