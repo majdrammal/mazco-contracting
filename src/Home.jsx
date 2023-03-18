dc8f6c3a8904b8dc4224a18c374d2a4f6a204762
majdrammal <majdrammal2001@gmail.com>
2023-03-19 01:31:32 +0200

    visit btn + minor

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import React from 'react';
 import logo from './assets/logo.png';
 import globe from './assets/globe.png';
-import watchroom from './assets/feechTV.jpg'
+import watchroom from './assets/watchroom.PNG'
 
 const Home = () => {
     return (
@@ -37,26 +37,26 @@ const Home = () => {
                             <FontAwesomeIcon icon="fa-quote-right" />
                         </h1>
                         <p className="header__sub-title">Dedicated to investing in projects that promote the development of healthy green communities.</p>
-                        <button className="btn-1">About Us</button>
+                        <a href="#about"><button className="btn-1">About Us</button></a>
                     </div>
                 </div>
             </div>
             <div className="about">
                 <div className="about__container">
-                    <h1 className="about__title">About <span className="">Us</span></h1>
+                    <h1 className="about__title">About <span className="green">Us</span></h1>
                     <p className="about__text">Solid Trade is dedicated to investing in projects that promote the development of healthy green communities. 
 We believe that by investing in green projects and supporting healthy eateries, we can have a positive impact on the lifestyle of the communities in which we operate. By focusing on sustainability and environmentally-friendly practices, we strive to build a healthier world for future generations. 
 Our commitment to building healthy communities extends beyond just financial investments; we actively seek out opportunities to collaborate with local organizations and community leaders to promote wellness and support initiatives that improve the quality of life for everyone; for corporate social responsibility to sticking to standards that are the closest to being eco-friendly.
 At our core, we believe that by working together, we can create a better, healthier world for all.</p>
                 </div>
             </div>
+            <div className="separator">
+                <hr className="separator__left" />
+                <FontAwesomeIcon className="separator__icon" icon="fa-leaf" />
+                <hr className="separator__right" />
+            </div>
             <div className="sections">
                 <div className="sections__container">
-                    <div className="separator">
-                        <hr className="separator__left" />
-                        <FontAwesomeIcon className="separator__icon" icon="fa-leaf" />
-                        <hr className="separator__right" />
-                    </div>
                     <h1 className="sections__title"><span className="green">What</span> We Do</h1>
                     <div className="sections__cards">
                         <div className="sections__card">
@@ -65,6 +65,7 @@ At our core, we believe that by working together, we can create a better, health
                                 <h3 className="sections__card--title">Real Estate & Construction</h3>
                                 <p className="sections__card--sub-title green">Neom KSA</p>
                                 <p className="sections__card--para">We supply construction companies with sustainable construction materials, as well as other eco-friendly services to help develop green cities by implementing green systems within smart cities.</p>
+                                <a target="_blank" href="https://www.solidtrade.me"><button>Visit</button></a>
                             </div>
                         </div>
                         <div className="sections__card">
@@ -73,6 +74,7 @@ At our core, we believe that by working together, we can create a better, health
                                 <h3 className="sections__card--title">F&B</h3>
                                 <p className="sections__card--sub-title green">The Barn</p>
                                 <p className="sections__card--para">With the intention to make healthy eating easier and more accessible, we became co-founders of a healthy restaurant chain, which we intend to develop in more than 60 countries by the end of 2025.</p>
+                                <a target="_blank" href="https://www.thebarnbeirut.com"><button>Visit</button></a>
                             </div>
                         </div>
                         <div className="sections__card">
@@ -80,7 +82,8 @@ At our core, we believe that by working together, we can create a better, health
                             <div className="sections__card--text">
                                 <h3 className="sections__card--title">Logistics</h3>
                                 <p className="sections__card--sub-title green">Trux Logistics</p>
-                                <p className="sections__card--para">To minimize the environmental impact of logistics, we are shifting our fleet into eco-friendly transport options</p>
+                                <p className="sections__card--para">Owning an extensive fleet and a long proven track record in the African market, our African company has taken the decision to shift it's fleet into eco-friendly transport options to minimize the environmental impact of logistics.</p>
+                                <a target="_blank" href="https://www.truxlogistics.co"><button>Visit</button></a>
                             </div>
                         </div>
                         <div className="sections__card">
@@ -89,6 +92,7 @@ At our core, we believe that by working together, we can create a better, health
                                 <h3 className="sections__card--title">Technology</h3>
                                 <p className="sections__card--sub-title green">Watchroom</p>
                                 <p className="sections__card--para">In an effort to bring people closer together, we are involving ourselves in technological ventures that aim to bridge gaps and enhance connectivity, as well as comfortable living with technologies that improve home automation and energy efficiency.</p>
+                                <a target="_blank" href="https://www.watchroom.tv"><button>Visit</button></a>
                             </div>
                         </div>
                     </div>
